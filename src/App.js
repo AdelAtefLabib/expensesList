a35638d9b14fd5a0977362d8b763507248beb4f9
alabib <alabib@shory.com>
2023-01-18 15:48:10 +0200

    pass array to expenseItem

--- src/App.js
+++ src/App.js
@@ -24,24 +24,7 @@ const expenses = [
 const App = () => {
   return (
     <div>
-      <ExpenseItem
-        id={expenses[0].id}
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      />
-      <ExpenseItem
-        id={expenses[1].id}
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      />
-      <ExpenseItem
-        id={expenses[2].id}
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      />
+      <ExpenseItem expenses={expenses} />
     </div>
   );
 };
