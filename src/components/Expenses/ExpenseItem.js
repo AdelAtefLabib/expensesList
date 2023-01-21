6889c5f4427377b71e64a92dc08f68e43fc04c63
Adel Labib <42178616+AdelAtefLabib@users.noreply.github.com>
2023-01-21 21:45:56 +0200

    add EventsAndState

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,14 +1,21 @@
 import "./ExpenseItem.css";
+import React,{useState} from "react";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 const ExpenseItem = (props) => {
+  const[title,setTitle]= useState(props.item.title)
+  const handleClick=()=>{
+    setTitle('updated')
+  }
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.item.date} />
       <div className="expense-item__description">
-        <h2>{props.item.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.item.amount}</div>
       </div>
+      <button onClick={handleClick}>Click</button>
+
     </Card>
   );
 };
