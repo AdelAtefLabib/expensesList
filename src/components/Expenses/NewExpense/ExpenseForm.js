c4eeef42b6e0e5feee6d9678af7761fc1ac07bfc
Adel Labib <42178616+AdelAtefLabib@users.noreply.github.com>
2023-01-22 17:33:13 +0200

    AddExpense

---
+++ src/components/Expenses/NewExpense/ExpenseForm.js
@@ -0,0 +1,9 @@
+import React from "react";
+const ExpenseForm=()=>{
+return(
+    <div className="new-expense">
+        <input ></input>
+    </div>
+)
+}
+export default ExpenseForm
---
+++ src/components/Expenses/NewExpense/NewExpense.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ExpenseForm from "./ExpenseForm";
+const NewExpense=()=>{
+    return(
+         <ExpenseForm/>
+    )
+}
+export default NewExpense
